0786f7d53ff66fabaafd62df3ed7dbd6e7ae37db
Rob <child@ocdstudio.net>
2015-02-09 09:06:41 +0100

    fixed test

--- test/test.js
+++ test/test.js
@@ -2,10 +2,10 @@ var assert = require("assert")
 var request = require("supertest")
 var app = require('../app');
   describe('calling root url', function(){
-    it('should return 500', function(done){
+    it('should return 200', function(done){
      request(app)
         .get('/')
-        .expect(500,done);
+        .expect(200,done);
     })
   })
 
